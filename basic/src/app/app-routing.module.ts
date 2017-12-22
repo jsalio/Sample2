a9366d7862abea5d48ac1095ab83f538b8886774
Unknown <tecno12jorge@gmail.com>
2017-12-22 13:27:14 -0400

    Adding Bootstrap Style

--- basic/src/app/app-routing.module.ts
+++ basic/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { TemplateComponent } from './template/template/template.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: TemplateComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- basic/src/app/app.module.ts
+++ basic/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TemplateComponent } from './template/template/template.component';
+import { TopNavComponent } from './template/top-nav/top-nav.component';
+import { FooterComponent } from './template/footer/footer.component';
+import { AsideMenuComponent } from './template/aside-menu/aside-menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TemplateComponent,
+    TopNavComponent,
+    FooterComponent,
+    AsideMenuComponent
   ],
   imports: [
     BrowserModule,
